62c1101903dc2477d4ea3adef46ed8e820729d76
ddruganov <ddruganov@bk.ru>
2022-01-30 14:58:32 +0300

    added db config

--- common/config/main.php
+++ common/config/main.php
@@ -1,19 +1,31 @@
 <?php
 
+use yii\db\Connection;
 use yii\log\FileTarget;
 
 return [
     'vendorPath' => Yii::getAlias('@vendor'),
     'components' => [
         'log' => [
-            [
-                'class' => FileTarget::class,
-                'categories' => ['application'],
-                'logVars' => [],
-                'logFile' => '@logs/main.log',
-                'enableRotation' => false,
-                'prefix' => fn () => ''
-            ]
-        ]
+            'targets' => [
+                [
+                    'class' => FileTarget::class,
+                    'categories' => ['application'],
+                    'logVars' => [],
+                    'logFile' => '@logs/main.log',
+                    'enableRotation' => false,
+                    'prefix' => fn () => ''
+                ]
+            ],
+        ],
+        'db' => [
+            'class' => Connection::class,
+            'dsn' => 'pgsql:host=yii2-api-template-db;dbname=pgdb',
+            'username' => 'pguser',
+            'password' => 'pgpass',
+            'charset' => 'utf8',
+            'enableSchemaCache' => false,
+        ],
     ],
+    'params' => require 'params.php',
 ];
